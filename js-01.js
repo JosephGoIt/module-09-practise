f6cf2b243ff575bd839d793197ffa03a970eec19
Joseph Camacho <spongkj@gmail.com>
2024-02-18 21:26:20 +0800

    test

--- js-01.js
+++ js-01.js
@@ -1,3 +1,15 @@
-const message = "Hello world";
-console.log(message);
-alert ("test");
+const button = document.querySelector("button");
+
+const onClick = () => {
+  const timerId = setTimeout(() => {
+    console.log("I love async JS!");
+  }, 2000);
+
+  console.log(timerId);
+  console.log("Wait for it");
+  console.log("It's gonna come");
+  console.log("I called it 2 secs ago");
+  console.log("There you go");
+};
+
+button.addEventListener("click", onClick);
